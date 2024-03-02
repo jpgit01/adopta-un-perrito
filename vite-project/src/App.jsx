c407ca5e585cf2d9ea64643f7f011a99cd857b07
Juan Pablo M. <pablochileidh@gmail.com>
2024-03-02 15:48:51 -0300

    avances

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -10,7 +10,26 @@ function App() {
   return (
     <>
     <Header titulo='Adopta Un Perrito'/>
-    <Card img='https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg' nombre='Dago' descripcion='Lindo perrito'/>
+    <Card 
+          img='https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg' 
+          nombre='Alma' 
+          descripcion='Lindo perrito'
+    />
+        <Card 
+          img='https://images.dog.ceo/breeds/hound-afghan/n02088094_1004.jpg' 
+          nombre='Barto' 
+          descripcion='Lindo perrito'
+    />
+        <Card 
+          img='https://images.dog.ceo/breeds/hound-afghan/n02088094_1005.jpg' 
+          nombre='Cano' 
+          descripcion='Lindo perrito'
+    />
+        <Card 
+          img='https://images.dog.ceo/breeds/hound-afghan/n02088094_1006.jpg' 
+          nombre='Dago' 
+          descripcion='Lindo perrito'
+    />
     <Footer/>
     </>
   )
