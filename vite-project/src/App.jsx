7c9b657d82490c68f50ce42a7398c6fb7b41847e
Juan Pablo M. <pablochileidh@gmail.com>
2024-03-02 15:45:45 -0300

    avances

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,13 +2,15 @@ import './App.css'
 import Header from './compnents/header'
 import Footer from './compnents/footer'
 import Card from './compnents/card'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
 
   return (
     <>
-    <Header titulo='Adopta un Perrito'/>
-    <Card/>
+    <Header titulo='Adopta Un Perrito'/>
+    <Card img='https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg' nombre='Dago' descripcion='Lindo perrito'/>
     <Footer/>
     </>
   )
--- vite-project/src/compnents/card.jsx
+++ vite-project/src/compnents/card.jsx
@@ -1,11 +1,20 @@
-function Card() {
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-    return (
-      <>
-      <h1>Card</h1>
-      </>
-    )
-  }
-  
-  export default Card
+function CardUno({img,nombre,descripcion}) {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={img} />
+      <Card.Body>
+        <Card.Title>{nombre}</Card.Title>
+        <Card.Text>
+          {descripcion}
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default CardUno;
   
